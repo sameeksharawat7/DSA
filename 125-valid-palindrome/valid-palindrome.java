05f3f89e72dee0cd03dcfaf70321103d40768a80
sameeksharawat7 <rawatsameeksha9@gmail.com>
2026-02-05 01:40:00 +0530

    Time: 2 ms (98.85%) | Memory: 44.5 MB (66.23%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,38 +1,66 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb=new StringBuilder();
-        int i=0;
-        while(i<s.length()){
-            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
-                sb.append((char)(s.charAt(i)+32));
-                i++;
-            }
-            else if(s.charAt(i)>='a' && s.charAt(i)<='z'){
-                sb.append(s.charAt(i));
-                i++;
-            }
-            else if(s.charAt(i)>='0' && s.charAt(i)<='9'){
-                sb.append(s.charAt(i));
-                i++;
-            }
-            else{
-                i++;
-            }
+    //     StringBuilder sb=new StringBuilder();
+    //     int i=0;
+    //     while(i<s.length()){
+    //         if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+    //             sb.append((char)(s.charAt(i)+32));
+    //             i++;
+    //         }
+    //         else if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+    //             sb.append(s.charAt(i));
+    //             i++;
+    //         }
+    //         else if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+    //             sb.append(s.charAt(i));
+    //             i++;
+    //         }
+    //         else{
+    //             i++;
+    //         }
 
-        }
-        return palindrome(sb);
-    }
+    //     }
+    //     return palindrome(sb);
+    // }
 
-    public boolean palindrome(StringBuilder sb){
+    // public boolean palindrome(StringBuilder sb){
+    //     int i=0;
+    //     int j=sb.length()-1;
+    //     while(i<j){
+    //         if(sb.charAt(i)!=sb.charAt(j)){
+    //             return false;
+    //         }
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+
         int i=0;
-        int j=sb.length()-1;
+        int j=s.length()-1;
         while(i<j){
-            if(sb.charAt(i)!=sb.charAt(j)){
+            while(i<j && !isAlphanumeric(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !isAlphanumeric(s.charAt(j))){
+                j--;
+            }
+            if(toLower(s.charAt(i))!=toLower(s.charAt(j))){
                 return false;
             }
             i++;
             j--;
         }
         return true;
+    }
+
+    public boolean isAlphanumeric(char c){
+        return (c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9');
+    }
+
+    public char toLower(char c){
+        if(c>='A' && c<='Z'){
+            return (char)(c+32);
+        }
+        return c;
     }
 }
