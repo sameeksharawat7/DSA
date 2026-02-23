ed30fe82e61e2198db94d46c022f406ec1483326
sameeksharawat7 <rawatsameeksha9@gmail.com>
2026-02-23 21:39:37 +0530

    Time: 2 ms (93.59%) | Memory: 47.1 MB (73.90%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,29 +1,24 @@
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
         ArrayList<Integer> res=new ArrayList<>();
-        ArrayList<Integer> maxcol=new ArrayList<>();
-        ArrayList<Integer> minrow=new ArrayList<>();
         int rows=matrix.length;
         int cols=matrix[0].length;
         for(int i=0;i<rows;i++){
-            int min=matrix[i][0];
+            int mincol=0;
             for(int j=0;j<cols;j++){
-                min=Math.min(min,matrix[i][j]);
-            }
-            minrow.add(min);
-        }
-        for(int i=0;i<cols;i++){
-            int max=matrix[0][i];
-            for(int j=0;j<rows;j++){
-                max=Math.max(max,matrix[j][i]);
-            }
-            maxcol.add(max);
-        }
-        for (int i = 0; i < maxcol.size(); i++) {
-            for (int j = 0; j < minrow.size(); j++) {
-                if (maxcol.get(i).equals(minrow.get(j))) {
-                    res.add(maxcol.get(i));
+                if(matrix[i][j]<matrix[i][mincol]){
+                    mincol=j;
                 }
+            }
+            boolean isLucky=true;
+            for(int k=0;k<rows;k++){
+                if(matrix[k][mincol]>matrix[i][mincol]){
+                    isLucky=false;
+                    break;
+                }
+            }
+            if(isLucky){
+                res.add(matrix[i][mincol]);
             }
         }
         return res;
