80ea3e1e6b358a6414503381fe213014b724edb8
sameeksharawat7 <rawatsameeksha9@gmail.com>
2026-04-01 01:05:51 +0530

    Time: 5 ms (57.25%) | Memory: 53.5 MB (88.26%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,17 +1,21 @@
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> res=new ArrayList<>();
-        res.add(0);
-        if(n==0){
-            return res;
-        }
-        res.add(1);
-        int cur=1;
-        for(int i=2;i<=n;i++){
-            cur=cur*2;
-            for(int j=res.size()-1;j>=0;j--){
-                res.add(cur+res.get(j));
-            }
+        // res.add(0);
+        // if(n==0){
+        //     return res;
+        // }
+        // res.add(1);
+        // int cur=1;
+        // for(int i=2;i<=n;i++){
+        //     cur=cur*2;
+        //     for(int j=res.size()-1;j>=0;j--){
+        //         res.add(cur+res.get(j));
+        //     }
+        // }
+        // return res;
+        for(int i=0;i<(1<<n);i++){
+            res.add(i^(i>>1));
         }
         return res;
     }
