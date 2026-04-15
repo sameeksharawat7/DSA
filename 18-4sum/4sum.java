09dc24421de4e154e362574ba55ab666f8ba3200
sameeksharawat7 <rawatsameeksha9@gmail.com>
2026-04-15 16:55:07 +0530

    Time: 17 ms (92.37%) | Memory: 45.8 MB (66.22%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -23,14 +23,14 @@ class Solution {
                     }
                     else{
                         result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        while(left<right && nums[left]==nums[left+1]){
-                            left++;
-                        }
-                        while(left<right && nums[right]==nums[right-1]){
-                            right--;
-                        }
                         left++;
                         right--;
+                        while(left<right && nums[left]==nums[left-1]){
+                            left++;
+                        }
+                        while(left<right && nums[right]==nums[right+1]){
+                            right--;
+                        }
                     }
 
                 }
