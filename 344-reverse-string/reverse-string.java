fabde2a6e3ae507adb5ac130d8ceb962443bfddc
sameeksharawat7 <rawatsameeksha9@gmail.com>
2026-04-20 13:02:44 +0530

    Time: 1 ms (22.32%) | Memory: 48.8 MB (6.57%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -3,15 +3,12 @@ class Solution {
         recursiveReverse(s,0,s.length-1);
     }
     void recursiveReverse(char[] s,int i,int j){
-        if(i>j){
+        if(i>=j){
             return;
         }
-        swap(s,i,j);
-        recursiveReverse(s,i+1,j-1);
-    } 
-    void swap(char[] s,int i,int j){
         char temp=s[i];
         s[i]=s[j];
         s[j]=temp;
-    } 
+        recursiveReverse(s,i+1,j-1);
+    }  
 }
