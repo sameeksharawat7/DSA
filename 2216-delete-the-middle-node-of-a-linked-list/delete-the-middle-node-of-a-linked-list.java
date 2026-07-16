24598f4fe370e1befd3f4533fa93f7b606be5963
sameeksharawat7 <rawatsameeksha9@gmail.com>
2026-07-16 12:27:28 +0530

    Time: 3 ms (99.96%) | Memory: 202.6 MB (39.12%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -8,26 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ //O(n+n/2)
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         ListNode temp=head;
+//         int length=0;
+//         while(temp!=null){
+//             length++;
+//             temp=temp.next;
+//         } 
+//         if(length==1){
+//             return null;
+//         }
+//         int mid=length/2;
+//         temp=head;
+//         while(mid>1){
+//             temp=temp.next;
+//             mid-=1;
+//         }
+//         if(temp.next!=null){
+//             temp.next=temp.next.next;
+//         }
+//         return head;
+//     }
+// }
+
+//o(n) fast and slow pointer
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        ListNode temp=head;
-        int length=0;
-        while(temp!=null){
-            length++;
-            temp=temp.next;
-        } 
-        if(length==1){
+        if (head == null || head.next == null){
             return null;
         }
-        int mid=length/2;
-        temp=head;
-        while(mid>1){
-            temp=temp.next;
-            mid-=1;
+        ListNode fast=head;
+        ListNode slow=head;
+        fast=fast.next.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        if(temp.next!=null){
-            temp.next=temp.next.next;
-        }
+        slow.next=slow.next.next;
         return head;
     }
 }
