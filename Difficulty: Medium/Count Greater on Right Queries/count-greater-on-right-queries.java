7ebe07d1ea12fcda3441cfc54e09bf3d7aba493d
sameeksharawat7 <rawatsameeksha9@gmail.com>
2026-07-24 15:16:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Greater on Right Queries/count-greater-on-right-queries.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] countGreater(int arr[], int indices[]) {
+        int[] ans=new int[indices.length];
+        for(int i=0;i<indices.length;i++){
+            int idx=indices[i];
+            int count=0;
+            for(int j=idx+1;j<arr.length;j++){
+                if(arr[idx]<arr[j]){
+                    count++;
+                }
+                ans[i]=count;
+            }
+        }
+        return ans;
+    }
+}
